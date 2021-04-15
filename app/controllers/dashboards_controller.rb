cd7fc69d174589142159485d055c78556cba00cf
Elian Rojas <impresosgosen@gmail.com>
2021-04-14 22:23:53 -0600

    Graphic by months ready

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,12 @@
 class DashboardsController < ApplicationController
   def index
-      @coffees_in_months =  Coffee.group_by_month(:created_at).count
+      
+      @coffees_in_months = Coffee.group_by_month(:created_at, last: 12).sum(:price)
+      @coffees_by_count = Coffee.group_by_month(:created_at, last: 12).count
+      @coffees_by_prom = Coffee.group_by_month(:created_at, last: 12).average(:price)
+
+    
+
+
   end
 end
