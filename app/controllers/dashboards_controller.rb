395aaa5e19090d2dcbf6a785b83848137550bd66
Elian Rojas <impresosgosen@gmail.com>
2021-04-15 23:53:19 -0600

    Graphics ready

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,9 +4,13 @@ class DashboardsController < ApplicationController
       @coffees_in_months = Coffee.group_by_month(:created_at, last: 12).sum(:price)
       @coffees_by_count = Coffee.group_by_month(:created_at, last: 12).count
       @coffees_by_prom = Coffee.group_by_month(:created_at, last: 12).average(:price)
-
-    
-
-
+      @coffees_by_origin_year = Coffee.where(created_at: 11.months.ago..Date.today).group(:origin).count
+      @coffees_by_origin_6month = Coffee.where(created_at: 5.months.ago..Date.today).group(:origin).count
+      @coffees_by_origin_last3month = Coffee.where(created_at: 2.months.ago..Date.today).group(:origin).count
+      @coffees_by_origin_lastmonth = Coffee.where(created_at: 1.months.ago..Date.today).group(:origin).count
+      @coffees_by_blend_year = Coffee.where(created_at: 11.months.ago..Date.today).group(:blend).count
+      @coffees_by_blend_6month = Coffee.where(created_at: 5.months.ago..Date.today).group(:blend).count
+      @coffees_by_blend_last3month = Coffee.where(created_at: 2.months.ago..Date.today).group(:blend).count
+      @coffees_by_blend_lastmonth = Coffee.where(created_at: 1.months.ago..Date.today).group(:blend).count
   end
 end
